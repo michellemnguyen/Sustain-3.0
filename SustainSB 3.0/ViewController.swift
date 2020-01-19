04332497743483a85df0e4334d2770865d9cb9a5
michellemnguyen <michellemnguyen@ucsb.edu>
2020-01-19 02:43:42 -0800

    Initial Commit

---
+++ SustainSB 3.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SustainSB 3.0
+//
+//  Created by Michelle Nguyen on 1/19/20.
+//  Copyright © 2020 Michelle Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
